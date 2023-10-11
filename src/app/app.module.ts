3e7754932bf8ecf2085457cf12ac8e6241ce883c
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-10-11 15:43:37 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AvaliarComponent } from './components/avaliar/avaliar.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GerenciarComponent } from './components/gerenciar/gerenciar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuInicialComponent } from './components/menu-inicial/menu-inicial.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { HomeUserComponent } from './components/home-user/home-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvaliarComponent,
+    CadastroComponent,
+    FooterComponent,
+    GerenciarComponent,
+    HeaderComponent,
+    HeaderUserComponent,
+    LoginComponent,
+    MenuInicialComponent,
+    PerfilComponent,
+    ReservaComponent,
+    HomeUserComponent
   ],
   imports: [
     BrowserModule,
