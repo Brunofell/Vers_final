5921c049f9c696806ca2e4a3c8a61359a914d61f
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-10-11 15:51:41 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { MenuInicialComponent } from './components/menu-inicial/menu-inicial.component';
+import { AvaliarComponent } from './components/avaliar/avaliar.component';
+import { HomeUserComponent } from './components/home-user/home-user.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { GerenciarComponent } from './components/gerenciar/gerenciar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'reserva', component: ReservaComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'menu', component: MenuInicialComponent},
+  {path: 'avaliar', component: AvaliarComponent},
+  {path: 'homeUser', component: HomeUserComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: 'gerenciar', component: GerenciarComponent},
+
+  // aqui redirecionando quando abrir a aplicação
+  { path: '', redirectTo: '/menu', pathMatch: 'full' }, // Redireciona para a rota '/menu'
+  { path: 'menu', component: MenuInicialComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
